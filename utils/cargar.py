72c03fb5df9c478a9740ca77fdc384b2dd885187
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2023-05-25 19:19:47 -0400

    update datamap fases

--- utils/cargar.py
+++ utils/cargar.py
@@ -3,13 +3,39 @@ import pandas as pd
 import os
 
 
-# Read YAML file - source: ChatGPT
+# Merge dictionaries - source: ChatGPT
+def merge_dictionaries(list_of_dictionaries):
+    merged_dict = {}
+    
+    for dictionary in list_of_dictionaries:
+        merged_dict.update(dictionary)
+    
+    return merged_dict
+
+
 with open('datamap.yaml', 'r') as stream:
     datamap = yaml.safe_load(stream)
+    for k in datamap.keys():
+        if '_fases' in k:
+            datamap[k] = merge_dictionaries(datamap[k])
+    for k in datamap.keys():
+        if '_fases' in k:
+            for kk in datamap[k].keys():
+                datamap[k][kk] = merge_dictionaries(datamap[k][kk])
 
 
 def files_in_folder(folder):
     return [file for file in os.listdir(folder) if '.csv' in file]
+
+
+def iteraciones_datamap(nombre):
+    if '_fases' in nombre:
+        raise KeyError("La variable nombre deben ser alguna entre 'adela', 'alicia', 'julieta' y 'laura'")
+    try:
+        iteraciones = datamap[nombre]
+    except KeyError:
+         raise KeyError("La variable nombre deben ser alguna entre 'adela', 'alicia', 'julieta' y 'laura'")
+    return iteraciones
 
 
 def df_caso(nombre:str) -> pd.DataFrame:
@@ -26,16 +52,30 @@ def df_caso(nombre:str) -> pd.DataFrame:
         df: pd.DataFrame
             un dataframe de pandas con las respuestas de todos los cursos y secciones
     """
-    try:
-        iteraciones = datamap[nombre]
-    except KeyError:
-         raise KeyError("La variable nombre deben ser alguna entre 'adela', 'alicia', 'julieta' y 'laura'")
+    iteraciones = iteraciones_datamap(nombre)
     dfs = []
     for curso in iteraciones:
             folder = datamap['parent']+curso+'/'
             files = files_in_folder(folder)
-            dfs += [pd.read_csv(folder + file, delimiter=';',index_col='id') for file in files]
+            for file in  files:
+                df = pd.read_csv(folder + file, delimiter=';',index_col='id')
+                df['curso'] = curso
+                dfs.append(df)
     df = pd.concat(dfs)
     df = df.drop(columns=['name','rut'])
 
     return df
+
+
+def fase(df:pd.DataFrame,fase:int,caso:str) -> pd.DataFrame:
+    """
+     Filtra un dataframe...
+    """
+    condition_tuples = []
+    key_caso = caso + '_fases'
+
+    for curso in datamap[key_caso].keys():
+        for indice in datamap[key_caso][curso][fase]:
+            condition_tuples.append((curso,indice))
+    
+    return df[(df['phase'],df['curso']) in condition_tuples]
