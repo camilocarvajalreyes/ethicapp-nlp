ef43484d7cff36d8aaa9d6840b63814fd5eb4ed8
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2023-06-17 10:24:24 -0400

    move preprocessing to script

--- utils/preprocesamiento.py
+++ utils/preprocesamiento.py
@@ -1,5 +1,13 @@
 """Text pre-processing utils"""
 import pandas as pd
+from nltk import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+stop_words = stopwords.words('spanish')
 
 
 def delete_non_string_rows(df:pd.DataFrame, column_name:str, verbose=True) -> pd.DataFrame:
@@ -29,3 +37,50 @@ def delete_non_string_rows(df:pd.DataFrame, column_name:str, verbose=True) -> pd
     df_removed = df.drop(non_string_rows.index)
 
     return df_removed
+
+
+class StemmerTokenizer:
+    def __init__(self):
+        self.ps = SnowballStemmer('spanish')
+    
+    def __call__(self, doc):
+        doc_tok = word_tokenize(doc)
+        doc_tok = [t for t in doc_tok if t not in stop_words]
+        return [self.ps.stem(t) for t in doc_tok]
+
+
+def make_BoW_preprocess(tokenizer:StemmerTokenizer,column:str,max_ngram:int=2,min_ngram:int=1) -> ColumnTransformer:
+    """
+    Wraps up tokenising and n_gram selection into a ColumnTransformer for a dataframe
+
+    Arguments
+
+        tokenizer: StemmerTokenizer
+            Instance of a custom class StemmerTokenizer, which reloves stop words and keeps the stem of words
+
+        column: str
+            target column name (from df) with text content
+        
+        max_ngram: int, default 2
+            maximum n_gram to consider as features
+
+        min_ngram: int, default 1
+            minimum ngram to consider, 1 being single words
+    
+    Returns
+    
+        preprocessing: ColumnTransformer
+            ColumnTransformer that should be put in a scikit-learn pipeline
+
+    """
+    
+    bog = CountVectorizer(
+        tokenizer = tokenizer,
+        ngram_range=(min_ngram,max_ngram)
+        )
+
+    preprocessing = ColumnTransformer(
+        transformers=[('bag-of-words',bog,column)]
+    )
+
+    return preprocessing
