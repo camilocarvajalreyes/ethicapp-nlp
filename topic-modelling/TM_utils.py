6ea67f92eb84575f57d3a643b4bb74118bea564a
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2023-07-26 09:49:19 -0400

    update Bertopic top k topicos por postura

--- topic-modelling/TM_utils.py
+++ topic-modelling/TM_utils.py
@@ -6,3 +6,17 @@ def get_lda_embeddings(lda_model,tokenizer,dictionary,df_test,column):
     doc_term_matrix = [dictionary.doc2bow(bow) for bow in tokenized_test]
     topic_probs = lda_model.get_document_topics(doc_term_matrix,minimum_probability=0)
     return np.array([[p[1] for p in probs] for probs in topic_probs])
+
+
+def get_table_top_topics(model,sim_topics,similarity,max_w=5):
+    # returns a table with max_w words representing the top topics given a topic list with similarity
+    topic_num = len(sim_topics)
+    data = [[] for _ in range(topic_num)]
+    for i, t in enumerate(sim_topics):
+        data[i].append(t)
+        data[i].append(similarity[i])
+        data[i] += [tup[0] for tup in model.get_topic(t)[:max_w]]
+
+    headers = ['Tópico','Probabilidad'] + ['Palabra {}'.format(i+1) for i in range(max_w)]
+
+    return headers, data
